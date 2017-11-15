b734222fff8edd064cee761fe81f49c1cd7f281e
Elena Czubiak <elenaczubiak@gmail.com>
2017-11-15 11:17:03 -0800

    Remove hostname from server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 
-const hostname = '127.0.0.1';
+// const hostname = '127.0.0.1';
 const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
@@ -9,6 +9,7 @@ const server = http.createServer((req, res) => {
   res.end('Hello World\n');
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, () => {
+  console.log(`Server running at port:${port}/`);
+  // console.log(`Server running at http://${hostname}:${port}/`);
 });
