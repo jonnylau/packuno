bec8a318720aaac626cc4fe3994a1dc93be2345c
Elena Czubiak <elenaczubiak@gmail.com>
2017-11-16 15:28:47 -0800

    Add reducer functions and added folder for components

---
+++ client/src/reducers/home.reducers.jsx
@@ -0,0 +1,31 @@
+
+const packingItem = (state, action) => {
+
+  if (action.type === 'ADD_ITEM') {
+    return [...state,
+      {
+        id: action.id, 
+        item: action.item, 
+        category: action.category;
+        packed:false
+      }]
+
+  } else if (action.type === 'TOGGLE_PACKED') {
+    return state.map((packingItem) => {
+      if (packingItem.id !== action.id) {
+        return packingItem;
+      }
+
+      return Object.assign( {}, packingItem, {packed: !packingItem.packed});
+
+    });
+
+  } else if (action.type === 'SET_CAT_FILTER') {
+
+    return action.filter;
+    
+  } else {
+    return state;
+
+  }
+};
