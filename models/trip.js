851adc75f764df4f394d088679227d29a8f4fdb3
elena-cz <elenaczubiak@gmail.com>
2017-11-16 15:26:28 -0800

    Update trip.js

--- models/trip.js
+++ models/trip.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
       type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV4,
     },
-    Destination: DataTypes.STRING,
+    destination: DataTypes.STRING,
     start_date: {
       type: DataTypes.DATE,
       allowNull: false,
