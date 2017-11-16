12bd918e18f5c5a8379f959418441acab820a88c
Elena Czubiak <elenaczubiak@gmail.com>
2017-11-16 14:29:03 -0800

    Add folders for Redux and React files, created basic actions for packing list

---
+++ client/src/reducers/home.reducers.jsx
@@ -0,0 +1,14 @@
+
+const packingListRed = (state, action) => {
+  if (action.type === 'ADD_ITEM') {
+
+
+  } else if (action.type === 'TOGGLE_PACKED') {
+
+  } else if (action.type === 'SET_CAT_FILTER') {
+
+  } else {
+    return state;
+    
+  }
+};
