29474149e4b246ec0c6174013d8bd531eb0fe025
Elena Czubiak <elenaczubiak@gmail.com>
2017-11-16 16:18:21 -0800

    Add presentational component for adding new items

---
+++ client/src/components/AddItemForm.component.jsx
@@ -0,0 +1,22 @@
+
+const AddItemForm = ({ onSubmit }) => {
+
+  let item_input;
+  let category_input;
+
+  return (
+    <div>
+      <form onSubmit={(e) => { onSubmit(item_input.value, category_input.value);}} >
+        <label>Item</label>
+        <input type="text" ref={(input) => {this.item_input = input;}} />
+        <label>Category</label>
+        <input type="text" ref={(input) => {this.category_input = input;}} />
+        <button></button>
+      </form>
+    </div>
+  )
+
+};
+
+
+export default AddItemForm;
