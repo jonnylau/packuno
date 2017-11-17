af7c3be55add2a09c6bdeead3c0fdc9d203879d7
Elena Czubiak <elenaczubiak@gmail.com>
2017-11-16 18:54:49 -0800

    Add top-level redux-react components

--- client/src/components/Link.component.jsx
+++ client/src/components/Link.component.jsx
@@ -1,8 +1,8 @@
-const CategoryLink = ({ active, children, onClick }) => {
+const Link = ({ active, children, onClick }) => {
   if (active) {
     return <span>{ children }</span>
   }
   return <a href="#" onClick= { (e) => { onClick() } } >{ children }</a>
 };
 
-export default CategoryLink;
+export default Link;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,1 +1,16 @@
+import 'babel-polyfill'
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import packingList from './reducers'
+import Home from './components/Home.component'
 
+let store = createStore(todoApp)
+
+render(
+  <Provider store={store}>
+    <Home />
+  </Provider>,
+  document.getElementById('home')
+)
