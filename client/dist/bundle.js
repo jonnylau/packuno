95c47fa5b78099f8960af68a4cf1ae15e1db1108
Elena Czubiak <elenaczubiak@gmail.com>
2017-11-20 23:01:35 -0800

    Remove bundle.js before pull request

--- client/dist/bundle.js
+++ client/dist/bundle.js
@@ -68,7 +68,7 @@
 /***/ 325:
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed: SyntaxError: Unexpected token, expected , (40:2)\n\n\u001b[0m \u001b[90m 38 | \u001b[39m    \u001b[33m<\u001b[39m\u001b[33m/\u001b[39m\u001b[33mRouter\u001b[39m\u001b[33m>\u001b[39m\n \u001b[90m 39 | \u001b[39m  \u001b[33m<\u001b[39m\u001b[33m/\u001b[39m\u001b[33mProvider\u001b[39m\u001b[33m>\u001b[39m\n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 40 | \u001b[39m  document\u001b[33m.\u001b[39mgetElementById(\u001b[32m'app'\u001b[39m)\n \u001b[90m    | \u001b[39m  \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 41 | \u001b[39m)\u001b[33m;\u001b[39m\u001b[0m\n");
+throw new Error("Module build failed: SyntaxError: Unexpected token, expected , (42:2)\n\n\u001b[0m \u001b[90m 40 | \u001b[39m    \u001b[33m<\u001b[39m\u001b[33m/\u001b[39m\u001b[33mRouter\u001b[39m\u001b[33m>\u001b[39m\n \u001b[90m 41 | \u001b[39m  \u001b[33m<\u001b[39m\u001b[33m/\u001b[39m\u001b[33mProvider\u001b[39m\u001b[33m>\u001b[39m\n\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m 42 | \u001b[39m  document\u001b[33m.\u001b[39mgetElementById(\u001b[32m'app'\u001b[39m)\n \u001b[90m    | \u001b[39m  \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\n \u001b[90m 43 | \u001b[39m)\u001b[33m;\u001b[39m\u001b[0m\n");
 
 /***/ })
 
