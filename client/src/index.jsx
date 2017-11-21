729132abcf48181f4e6e4766f6fcb6e27e89daf8
Luis Alvarez <lalvarez820@gmail.com>
2017-11-20 22:45:19 -0800

    merged

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -42,67 +42,8 @@ render(
   </Provider>,
   document.getElementById('app')
 );
-=======
-=======
->>>>>>> a7c7629d0a4ae7cb19ec82727d7584feda098148
-import React from 'react';
-import ReactDOM from 'react-dom';
 
-const handleClick = () => {
-  document.getElementById("sidebar").style.display = "none";
-  document.getElementById("content").style.marginLeft = "0%";
-  document.getElementById("openButton").innerHTML = "&equiv;";
-};
 
-const handleOpen = () => {
-  document.getElementById("sidebar").style.display = "block";
-  document.getElementById("content").style.marginLeft = "25%";
-  document.getElementById("openButton").innerHTML = "";
-};
-
-<<<<<<< HEAD
-
-const App = () => (
-  <div>
-    <div id="sidebar">
-      <button id="closeButton" onClick={handleClick}>&equiv;</button>
-      <h1> Packuno </h1>
-      <br />
-      <h2> Upcoming Trips </h2>
-    </div>
-    <div id="content">
-      <button id="openButton" onClick={handleOpen} />
-      <h1> Create New Trip </h1>
-      <br />
-      1. <input type="text" className="destination" placeholder="Enter destination" />
-      <br /><br />
-      2. <input type="date" className="departureDate" value="2017-11-29" />
-      <text> to </text>
-      <input type="date" className="returnDate" value="2018-01-01" />
-      <br /><br />
-      <button type="submit" className="submitButton"> Submit </button>
-      <br /><br /><br /><br />
-      3. Copy Packing List from Past Trips
-    </div>
-  </div>
-
-);
-
-ReactDOM.render(<App />, document.getElementById('app'));
-=======
-//When App renders, do a get request to our database to get the name and dates of 
-//an upcoming trip for the logged in user
-    //For each upcoming trip, do a get request to the Flickr API to render new pictures
-      //https://farm{farm-id}.staticflickr.com/{server-id}/{id}_{secret}.jpg
-      //rest endpoint: https://api.flickr.com/services/rest/
-
-//When App renders, do a get request to our database to get the name and dates of 
-//past trips for the logged in user. 
-    //For each old trip, do a get request to the Flickr API to render new pictures
-
-//When user clicks "Create Trip", app reroutes to trip.html, with new trip info rendered onto trip.html
-
-class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
