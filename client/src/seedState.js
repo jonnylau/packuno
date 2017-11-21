9d893cfdd422fc7e3347c9ee6bdd4ab8e9c69201
Elena Czubiak <elenaczubiak@gmail.com>
2017-11-20 23:00:34 -0800

    Add seedState with dummy data so app loads with a default state

--- client/src/seedState.js
+++ client/src/seedState.js
@@ -2,18 +2,18 @@ const seedState = {
   currentUserId: 1,
   currentTripId: 1,
   trips: {
-    byID: {
+    byId: {
       1: {
         id: 1,
-        destination: 'Hong Kong',
-        departureDate: '2017-9-29',
-        returnDate: '2017-11-01',
-      },
-      2: {
-        id: 2,
         destination: 'Mexico City',
         departureDate: '2018-01-07',
         returnDate: '2018-01-16',
+      },
+      2: {
+        id: 2,
+        destination: 'Hong Kong',
+        departureDate: '2017-9-29',
+        returnDate: '2017-11-01',
       },
       3: {
         id: 3,
@@ -28,10 +28,10 @@ const seedState = {
         returnDate: '2018-08-29',
       },
     },
-    allIDs: [1, 2, 3, 4],
+    allIds: [1, 2, 3, 4],
   },
   items: {
-    byID: {
+    byId: {
       1: {
         id: 1,
         item: 'Shampoo',
@@ -57,7 +57,7 @@ const seedState = {
         packed: false,
       },
     },
-    allIDs: [1, 2, 3, 4],
+    allIds: [1, 2, 3, 4],
   },
   visibilityFilter: 'SHOW_ALL',
   weatherWidget: 'HISTORICAL',
