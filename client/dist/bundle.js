e6f3400f7695d867007d21a9015f327d4fd40953
Luis Alvarez <lalvarez820@gmail.com>
2017-11-20 23:34:03 -0800

    added testing suite

--- client/dist/bundle.js
+++ client/dist/bundle.js
@@ -33418,6 +33418,7 @@ var isExtraneousPopstateEvent = function isExtraneousPopstateEvent(event) {
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.WeatherContainer = undefined;
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -33451,7 +33452,7 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var WeatherContainer = function (_React$Component) {
+var WeatherContainer = exports.WeatherContainer = function (_React$Component) {
   _inherits(WeatherContainer, _React$Component);
 
   function WeatherContainer(props) {
@@ -43474,11 +43475,6 @@ window.store = store;
     _react2.default.createElement(
       'div',
       null,
-      _react2.default.createElement(
-        'h2',
-        null,
-        'Hello'
-      ),
       _react2.default.createElement(
         'ul',
         null,
