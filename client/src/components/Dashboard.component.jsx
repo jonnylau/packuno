49e70965eeef50b91168d60c7c961eed0a7a9faf
Your Name <yu.hf.christina@gmail.com>
2017-11-23 00:12:23 +0800

    Dashboard old trips chnage color on mouseover, selected on click, and on submit of new trip info, redirects and sends post request

--- client/src/components/Dashboard.component.jsx
+++ client/src/components/Dashboard.component.jsx
@@ -1,45 +1,15 @@
 import React from 'react';
 import $ from 'jquery';
 import Moment from 'moment';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 import seedState from '../seedState.js';
+
 
 const handleClick = () => {
   document.getElementById("sidebar").style.display = "none";
   document.getElementById("content").style.marginLeft = "0%";
   document.getElementById("openButton").innerHTML = "&equiv;";
 };
-
-const handleOpen = () => {
-  document.getElementById("sidebar").style.display = "block";
-  document.getElementById("content").style.marginLeft = "25%";
-  document.getElementById("openButton").innerHTML = "";
-};
-
-const Dashboard = () => (
-  <div>
-    <div id="sidebar">
-      <button id="closeButton" onClick={handleClick}>&equiv;</button>
-      <h1> Packuno </h1>
-      <br />
-      <h2> Upcoming Trips </h2>
-    </div>
-    <div id="content">
-      <button id="openButton" onClick={handleOpen} />
-      <h1> Create New Trip </h1>
-      <br />
-      1. <input type="text" className="destination" placeholder="Enter destination" />
-      <br /><br />
-      2. <input type="date" className="departureDate" value="2017-11-29" />
-      <text> to </text>
-      <input type="date" className="returnDate" value="2018-01-01" />
-      <br /><br />
-      3. Copy Packing List from Past Trips
-      <br /><br />
-      <button type="submit" className="submitButton"> Create Trip </button>
-      <br /><br /><br /><br />
-    </div>
-  </div>
-);
 
 const handleOpen = () => {
   document.getElementById("sidebar").style.display = "block";
@@ -55,9 +25,86 @@ class Dashboard extends React.Component {
     super(props);
     this.state = {
       data: seedState,
+      pictures: [],
+      destination: '',
+      departureDate: '',
+      returnDate: '',
+      oldTripID: '',
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  componentDidMount() {
+    this.state.data.trips.allIds.forEach((element) => {
+      this.newPhotos(this.state.data.trips.byId[element].destination, (data) => {
+        const newArr = this.state.pictures;
+        newArr[element] = data;
+        this.setState({pictures: newArr});
+      });
+    });
+  }
+
+  handleChange(key) {
+    return function (e) {
+      var state = {};
+      state[key] = e.target.value;
+      this.setState(state);
+    }.bind(this);
+  }
+
+  handleSubmit(event) {
+    console.log('Destination', this.state.destination, 'Departure Date', this.state.departureDate, 'Return Date', this.state.returnDate, 'ID of Old Trip Selected', this.state.oldTripID);
+    //send a post request to the server to submit the above information
+    //$.post('url of server', {
+    //  destination: this.state.destination,
+    //  departureDate: this.state.departureDate,
+    //  returnDate: this.state.returnDate },
+    //  (err, data) => {
+    //  if (err) {
+    //    console.log('error fetching old trips', err);
+    //  }
+    //  console.log('success fetching old trips', data);
+    //  }, 'json')
+  }
+
+  identifyPicture(key){
+    this.setState({oldTripID: key});
+  }
+
+  onHover(event){
+    event.target.style.background = 'grey';
+  }
+
+  offHover(event){
+    event.target.style.background = 'Transparent';
+  }
+
+  newPhotos(country, cb) {
+    const xhr = new XMLHttpRequest();
+    if (!('withCredentials' in xhr)) {
+      alert('Browser does not support CORS.');
+      return;
+    };
+
+    xhr.open('GET', `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=59703335c51dcee9041f936cfa665b9f&tags=${country}, city, landmark&page=1&per_page=1&tag_mode=all`);
+
+    xhr.onload = () => {
+      const data = xhr.response;
+      const photoid = data.substring(data.indexOf('photo id') + 10, data.indexOf('" owner'));
+      const farmid = data.substring(data.indexOf('farm') + 6, data.indexOf('" title'));
+      const serverid = data.substring(data.indexOf('server') + 8, data.indexOf('farm=') - 2);
+      const secret = data.substring(data.indexOf('secret') + 8, data.indexOf(' server=') - 1);
+      const url = `https://farm${farmid}.staticflickr.com/${serverid}/${photoid}_${secret}.jpg`;
+      this.setState({images: url});
+      cb(url);
+    };
+
+    xhr.send();
+
+  };
+
+  //fetch old trip data from database;
   // fetchTrips() {
     // $.get('/alltrips', (err, data) => {
     //  if (err) {
@@ -82,7 +129,7 @@ class Dashboard extends React.Component {
               const dateLimit = Moment(this.state.data.trips.byId[element].returnDate);
               if (this.state.data.trips.byId[element].returnDate
                 != null && now.isBefore(dateLimit)) {
-                return (<li>
+                return (<li key={this.state.data.trips.byId[element].id}>
                   {this.state.data.trips.byId[element].destination}
                   <br />
                   From: {this.state.data.trips.byId[element].departureDate}
@@ -100,11 +147,11 @@ class Dashboard extends React.Component {
           <button id="openButton" onClick={handleOpen} />
           <h1> Create New Trip </h1>
           <br />
-          1. <input type="text" className="destination" placeholder="Enter destination" />
+          1. <input type="text" className="destination" placeholder="Enter destination" value={this.state.destination} onChange={this.handleChange('destination')}/>
           <br /><br />
-          2. <input type="date" className="departureDate" value="2017-11-29" />
+          2. <input type="date" className="departureDate" value={this.state.departureDate}  onChange={this.handleChange('departureDate')}/>
           <text> to </text>
-          <input type="date" className="returnDate" value="2018-01-01" />
+          <input type="date" className="returnDate" value={this.state.returnDate} onChange={this.handleChange('returnDate')}/>
           <br /><br />
           3. Copy Packing List from Past Trips
           <br /><br />
@@ -113,7 +160,9 @@ class Dashboard extends React.Component {
               {this.state.data.trips.allIds.map (element => {
               const dateLimit = Moment(this.state.data.trips.byId[element].returnDate);
               if (this.state.data.trips.byId[element].returnDate != null && now.isAfter(dateLimit)) {
-                return <li><a href="#">
+                return <li onClick={()=>this.identifyPicture(element)} onMouseEnter={this.onHover.bind(this)} onMouseLeave={this.offHover.bind(this)}><a href="#" value={element}>
+                  <img key={element} src={this.state.pictures[element]} className="image" />
+                  <br /><br />
                   {this.state.data.trips.byId[element].destination}
                   <br /><br />
                   From: {this.state.data.trips.byId[element].departureDate}
@@ -123,10 +172,11 @@ class Dashboard extends React.Component {
                 }
               }
             )}
+           <li onMouseEnter={this.onHover.bind(this)} onMouseLeave={this.offHover.bind(this)} onClick={()=>this.identifyPicture(0)}><a href="#"> <br /> (None) <br /><br /></a></li>
             </ul>
           </div>
           <br /><br />
-          <button type="submit" className="submitButton"> Create Trip </button>
+          <Link to="/trip"><button type="submit" className="submitButton" onClick={this.handleSubmit}> Create Trip </button></Link>
           <br /><br /><br /><br />
         </div>
       </div>
