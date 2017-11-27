dc173ccac43915a8422958687cef086ffc7f36eb
Luis Alvarez <lalvarez820@gmail.com>
2017-11-26 23:03:38 -0800

    changed Models for heroku

--- models/index.js
+++ models/index.js
@@ -9,7 +9,7 @@ const config = require('../config/config')[env];
 const db = {};
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  var sequelize = new Sequelize(process.env.DATABASE_URL);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
