c4514015095236d2df0c889aebdb0d1ee2d9588e
Luis Alvarez <lalvarez820@gmail.com>
2017-11-27 15:28:17 -0800

    fixed test suite

--- client/src/reducers/historical.reducers.jsx
+++ client/src/reducers/historical.reducers.jsx
@@ -1,6 +1,5 @@
 const setHistorical = (state = [], action) => {
   if (action.type === 'SET_HISTORICAL') {
-    console.log(JSON.parse(action.historical));
     return JSON.parse(action.historical);
   }
   return state;
